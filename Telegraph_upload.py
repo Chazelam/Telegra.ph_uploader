842b8df0c461756bd6e5c284e4c777b709b31a10
Chazelam <andreilaymzin@gmail.com>
2024-01-06 13:25:53 +0300

    telegraph_upload like lib

--- Telegraph_upload.py
+++ Telegraph_upload.py
@@ -2,20 +2,21 @@ from telegraph import Telegraph
 import requests
 import os
 
-telegraph = Telegraph()
-telegraph.create_account(short_name='1337')
-dir_name = "KuukoW - Nahida selfies 1"
-files = os.listdir(dir_name)
-images = []
+def uplload_folder(dir_name:str):
+    telegraph = Telegraph()
+    telegraph.create_account(short_name='1337')
+    
+    files = os.listdir(dir_name)
+    temp = []
+    for img in files:
+        with open(f"{dir_name}/{img}", 'rb') as f:
+            temp.append(requests.post('https://telegra.ph/upload', files={'file': ('file', f, 'image/jpeg')}).json()[0]['src'])
+    content = [f"<img src='{i}'/>" for i in temp]
+    
+    response = telegraph.create_page(dir_name, html_content="<p>My test page</p>" + "".join(content),)
+    return 'http://telegra.ph/{}'.format(response['path'])
 
-for img in files:
-    with open(f"{dir_name}/{img}", 'rb') as f:
-        images.append(requests.post('https://telegra.ph/upload', files={'file': ('file', f, 'image/jpeg')}).json()[0]['src'])
-        print(images)
-
-
-content = [f"<img src='{i}'/>" for i in images]
-
-response = telegraph.create_page(dir_name, html_content="<p>My test page</p>" + "".join(content),)
-
-print('http://telegra.ph/{}'.format(response['path']))
+if __name__ == "__main__":
+    dir_name = "KuukoW - Nahida selfies 1"
+    link = uplload_folder(dir_name)
+    print(link)
